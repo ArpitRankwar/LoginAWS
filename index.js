ba62db4bcb95a27918ff9946e37ab2e6ba2de228
mohitraj610 <mohitraj1999@gmail.com>
2022-04-23 17:08:11 +0530

    V6

--- index.js
+++ index.js
@@ -10,13 +10,10 @@ const authRoutes = require("./routes/auth");
 connection();
 
 // middlewares
-app.use(express.json());
-app.use(cors());
-
 app.use(function (req, res, next) {
 
     // Website you wish to allow to connect
-    res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000/');
+    res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');
 
     // Request methods you wish to allow
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
@@ -31,6 +28,9 @@ app.use(function (req, res, next) {
     // Pass to next layer of middleware
     next();
 });
+
+app.use(express.json());
+app.use(cors());
 
 // routes
 app.use("/api/users", userRoutes);
